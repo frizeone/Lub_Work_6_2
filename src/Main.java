830fb94aac12f152d495a491eef20aa35a69938d
Rostik_S <95083201+frizeone@users.noreply.github.com>
2023-10-27 12:05:30 +0300

    Update Main.java 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
     class Main{
         public static void main(String[] args) {
             System.out.print("Это добавлено в ветке main");
+
+
+            System.out.println("commit 2");
+
+
         }
     }
 
